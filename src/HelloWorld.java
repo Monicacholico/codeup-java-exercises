0fbd5b8561db187845591ad80160b9000c2d4c1c
monicacholico <monicachol@hotmail.com>
2018-04-30 10:57:48 -0500

    "Finished Java exercise"

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+        public static void main (String[] args) {
+            System.out.print("Hello, World!");
+            System.out.println("The shortcut to make a main method is psvm");
+            System.out.println("The shortcut for System.out.println is sout");
+        }
+}
+
